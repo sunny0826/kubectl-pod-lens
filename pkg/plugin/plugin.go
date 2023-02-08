8107d3ba700fe3828072ee33fa43dda9bcf96f61
Xudong Guo <guoxd@jihulab.com>
2023-02-08 16:05:26 +0800

    fix

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -130,14 +130,15 @@ func (sf *SnifferPlugin) getLabelByPod(labelFlag string) error {
 		labelSelector = "k8s-app=" + labels["k8s-app"]
 	} else if _, ok = labels["app.kubernetes.io/name"]; ok {
 		labelSelector = "app.kubernetes.io/name=" + labels["app.kubernetes.io/name"]
-	} else {
-		_, _ = cfmt.Println("Failed to get other, These labels do not exist:" +
-			" {{[release]}}::green" +
-			" {{[app]}}::green" +
-			" {{[k8s-app]}}::green" +
-			" {{[app.kubernetes.io/name]}}::green. So no related resources could be found.")
-		//os.Exit(1)
 	}
+	//else {
+	//	_, _ = cfmt.Println("Failed to get other, These labels do not exist:" +
+	//		" {{[release]}}::green" +
+	//		" {{[app]}}::green" +
+	//		" {{[k8s-app]}}::green" +
+	//		" {{[app.kubernetes.io/name]}}::green.")
+	//	os.Exit(1)
+	//}
 	sf.LabelSelector = labelSelector
 	return nil
 }
